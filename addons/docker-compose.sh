4366feb471ba91216d67a9ec0b0a9b5cd0864e93
Yann Le Doaré <yann@linuxconsole.org>
2024-12-14 17:45:05 +0100

    add docker-compose.sh

---
+++ addons/docker-compose.sh
@@ -0,0 +1,3 @@
+install -d $HOME/.local/bin
+curl -SL https://github.com/docker/compose/releases/download/v2.30.3/docker-compose-linux-x86_64 -o $HOME/.local/bin/docker-compose
+chmod +x $HOME/.local/bin/docker-compose
