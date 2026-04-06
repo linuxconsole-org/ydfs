10f01710729266ae80b57d614bdb118190356f3e
Yann Le Doaré <yann@linuxconsole.org>
2026-04-07 00:31:52 +0200

    grub menu

--- initrd/pivot-root.sh
+++ initrd/pivot-root.sh
@@ -1,6 +1,8 @@
 #https://man7.org/linux/man-pages/man2/pivot_root.2.html
+cp pivot_root_demo /tmp
+chmod +x /tmp/pivot_root_demo 
 mkdir /tmp/rootfs;
-gcc pivot_root_demo.c -o pivot_root_demo
+#gcc pivot_root_demo.c -o pivot_root_demo
 ls -id /tmp/rootfs;    # Show inode number of new root directory
 cp $(which busybox) /tmp/rootfs;
-PS1='bbsh$ ' sudo ./pivot_root_demo /tmp/rootfs /busybox sh;
+PS1='bbsh$ ' sudo /tmp/pivot_root_demo /tmp/rootfs /busybox sh;
